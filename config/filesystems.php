2f9baa525af6eab0b0310095194abc0fe468faad
Richard Florent <frichard@novacel-optical.com>
2018-02-21 15:53:18 +0100

    modif path image

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,7 +50,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path('/photo'),
+            'root' => public_path(),
             'visibility' => 'public',
         ],
 
